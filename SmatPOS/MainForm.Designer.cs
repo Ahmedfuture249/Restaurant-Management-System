b1500f389e0db8ae37aac49d99a0ad4538b5cbab
Ahmed Ali <A7medwhman@gmail.com>
2026-02-18 07:24:46 +0200

    Organizing the Project

---
+++ SmatPOS/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SmatPOS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.pnlogo = new System.Windows.Forms.Panel();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.btnReporting = new System.Windows.Forms.Button();
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.btnPOS = new System.Windows.Forms.Button();
+            this.pnMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.Gray;
+            this.pnMenu.Controls.Add(this.btnOptions);
+            this.pnMenu.Controls.Add(this.btnReporting);
+            this.pnMenu.Controls.Add(this.btnSetup);
+            this.pnMenu.Controls.Add(this.btnPOS);
+            this.pnMenu.Controls.Add(this.pnlogo);
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(261, 606);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // pnlogo
+            // 
+            this.pnlogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pnlogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlogo.Name = "pnlogo";
+            this.pnlogo.Size = new System.Drawing.Size(261, 102);
+            this.pnlogo.TabIndex = 0;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.BackColor = System.Drawing.Color.Gray;
+            this.btnOptions.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOptions.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.ForeColor = System.Drawing.Color.White;
+            this.btnOptions.Image = global::SmatPOS.Properties.Resources.gear;
+            this.btnOptions.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOptions.Location = new System.Drawing.Point(0, 342);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnOptions.Size = new System.Drawing.Size(261, 92);
+            this.btnOptions.TabIndex = 4;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOptions.UseVisualStyleBackColor = false;
+            // 
+            // btnReporting
+            // 
+            this.btnReporting.BackColor = System.Drawing.Color.Gray;
+            this.btnReporting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReporting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporting.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReporting.ForeColor = System.Drawing.Color.White;
+            this.btnReporting.Image = global::SmatPOS.Properties.Resources.report;
+            this.btnReporting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReporting.Location = new System.Drawing.Point(0, 266);
+            this.btnReporting.Name = "btnReporting";
+            this.btnReporting.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnReporting.Size = new System.Drawing.Size(261, 76);
+            this.btnReporting.TabIndex = 3;
+            this.btnReporting.Text = "Reporting";
+            this.btnReporting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReporting.UseVisualStyleBackColor = false;
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.BackColor = System.Drawing.Color.Gray;
+            this.btnSetup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetup.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetup.ForeColor = System.Drawing.Color.White;
+            this.btnSetup.Image = global::SmatPOS.Properties.Resources.settings;
+            this.btnSetup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetup.Location = new System.Drawing.Point(0, 184);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnSetup.Size = new System.Drawing.Size(261, 82);
+            this.btnSetup.TabIndex = 2;
+            this.btnSetup.Text = "Setup";
+            this.btnSetup.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSetup.UseVisualStyleBackColor = false;
+            // 
+            // btnPOS
+            // 
+            this.btnPOS.BackColor = System.Drawing.Color.Gray;
+            this.btnPOS.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPOS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPOS.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPOS.ForeColor = System.Drawing.Color.White;
+            this.btnPOS.Image = global::SmatPOS.Properties.Resources.shopping_cart;
+            this.btnPOS.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPOS.Location = new System.Drawing.Point(0, 102);
+            this.btnPOS.Name = "btnPOS";
+            this.btnPOS.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnPOS.Size = new System.Drawing.Size(261, 82);
+            this.btnPOS.TabIndex = 1;
+            this.btnPOS.Text = "Point Of Sale";
+            this.btnPOS.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPOS.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 606);
+            this.Controls.Add(this.pnMenu);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainForm";
+            this.Text = "Main SmartPOS";
+            this.pnMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel pnlogo;
+        private System.Windows.Forms.Button btnPOS;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Button btnReporting;
+        private System.Windows.Forms.Button btnSetup;
+    }
+}
+
